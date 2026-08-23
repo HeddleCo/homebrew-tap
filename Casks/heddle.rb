b7ecd664b6daa57654bfb88bcb5e66ff8c2421aa
forge-zephyr <261792963+forge-zephyr@users.noreply.github.com>
2026-08-23 20:57:54 +0000

    Update Heddle homebrew to 0.15.0

--- Casks/heddle.rb
+++ Casks/heddle.rb
@@ -1,6 +1,6 @@
 cask "heddle" do
-  version "0.12.0"
-  sha256 "8762a64e5b119c9a35d467a07843e0616a2456356ea8cea3e1f5a9a648a91c48"
+  version "0.15.0"
+  sha256 "2527d54df2fc20311cbfc4087ce972f6e6c749a080f02c4a4e52111fee261c01"
 
   url "https://github.com/HeddleCo/heddle/releases/download/v#{version}/Heddle-v#{version}-macos-universal.dmg",
       verified: "github.com/HeddleCo/heddle/"
